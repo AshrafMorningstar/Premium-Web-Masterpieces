269d68a71630ad50b77ebd4f1b2da572f769618c
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2026-01-03 17:05:11 +0530

    chore(copyright): update headers and license information © 2026

--- Infinity-Temperature-Converter/script.js
+++ Infinity-Temperature-Converter/script.js
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2026 Ashraf Morningstar
+ These are personal recreations of existing projects, developed by Ashraf Morningstar
+ for learning and skill development.
+ Original project concepts remain the intellectual property of their respective creators.
+ Repository: https://github.com/AshrafMorningstar
+*/
+
 /* Infinity Temperature Converter - Created by Ashraf Morningstar */
 
 console.log('Infinity Temperature Converter loaded successfully!');
